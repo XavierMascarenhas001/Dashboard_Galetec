a68359875d7695f0da6900072ba672f1963169b5
XavierMascarenhas001 <xavier.mascarenhas@gaeltecutilities.com>
2026-02-04 08:22:36 +0000

    Update Gaeltec2.0.py

--- Gaeltec2.0.py
+++ Gaeltec2.0.py
@@ -1766,10 +1766,10 @@ if misc_df is not None:
     # Data preparation
     # -----------------------------
     filtered_df['item'] = filtered_df['item'].astype(str)
-    misc_df['description'] = misc_df['description'].astype(str)
+    misc_df['Description'] = misc_df['Description'].astype(str)
 
     # Map items to work instructions
-    item_to_column_i = misc_df.set_index('description')['md poling'].to_dict()
+    item_to_column_i = misc_df.set_index('Description')['MD Poling'].to_dict()
     poles_df = filtered_df[filtered_df['pole'].notna() & (filtered_df['pole'].astype(str).str.lower() != "nan")].copy()
     poles_df['Work instructions'] = poles_df['item'].map(item_to_column_i)
 
